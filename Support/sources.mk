576b16d986435827500ccc6691c8c0eb3c92d4eb
Edwin Vans <vans.edw@gmail.com>
2024-05-11 15:02:54 -0700

    Create sources.mk

---
+++ Support/sources.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# File: sources.mk															 #
+# Author: Edwin Vans														 #
+# Date: 05/06/2024															 #
+#																			 #
+# Description: 																 #
+# ------------														 		 #
+# List all the source files for your project here   	 					 #
+# 																			 #
+##############################################################################
+
+SRCS := Firmware/main.c
